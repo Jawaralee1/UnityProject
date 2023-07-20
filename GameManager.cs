fa04e13ce563d2bc83ce62f01d740e77d1767453
LittleN645 <70450502+LittleN645@users.noreply.github.com>
2023-07-20 13:04:56 -0400

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private int gems = 0;
+    private int savedGems = 0;
+    [SerializeField] int health = 100;
+    public int maxHealth = 100;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    private void Update()
+    {
+        if(health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+    public int GetGems()
+    {
+        return gems;
+    }
+
+    public void AddGems(int amount)
+    {
+        gems += amount;
+    }
+
+    public int GetHealth()
+    {
+        return (int) (((double) health / maxHealth) * 100);
+    }
+
+    public void IncreaseMaxHealth(int amount)
+    {
+        maxHealth += amount;
+        health += amount;
+    }
+
+    public void ReduceHealth(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            maxHealth = 100;
+            health = 100;
+            gems = savedGems;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    public void SaveGems()
+    {
+        savedGems = gems;
+    }
+}
---
+++ HUDController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUDController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI gemsText;
+    [SerializeField] TextMeshProUGUI healthText;
+    void Update()
+    {
+        gemsText.text = "GEMS: " + GameManager.Instance.GetGems();
+        healthText.text = "HEALTH: " + GameManager.Instance.GetHealth() + "%";
+
+    }
+}
