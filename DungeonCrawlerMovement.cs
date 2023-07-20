60d102cfb25da17888aa81d4931dd1c65bf600cc
LittleN645 <70450502+LittleN645@users.noreply.github.com>
2023-07-20 15:15:04 -0400

    Update DungeonCrawlerMovement.cs

--- DungeonCrawlerMovement.cs
+++ DungeonCrawlerMovement.cs
@@ -1,39 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class DungeonCrawlerMovement : MonoBehaviour
 {
-    public float speed;
-    public float hp = 100;
+    Rigidbody2D rb;
+    public float movementSpeed;
+    public float projectileSpeed;
     public float shootTime;
+    public float numBullets;
     private float timer;
-
-    private Vector2 lastV = Vector2.down;
+    public List<GameObject> projectile;
     // Update is called once per frame
     void Update()
     {
-        Vector2 v = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-       //var anim = GetComponent<Animator>();
-       //anim.SetFloat("Horizontal", v.x);
-       //anim.SetFloat("Vertical", v.y);
-        if(v.magnitude > 0)
-        {
-            lastV = v.normalized;
-        }
-        v.Normalize();
-        GetComponent<Rigidbody2D>().velocity = v * speed;
+        //movement
+        rb = GetComponent<Rigidbody2D>();
 
-        if(hp == 0)
+        if (Input.GetKey(KeyCode.D))
         {
-           
+            rb.velocity = new Vector2(movementSpeed, 0);
         }
-        
-       
-        else if(timer > 0)
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-movementSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, movementSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0, -movementSpeed);
+        }
+
+        if (!(Input.GetKey(KeyCode.D)
+        || Input.GetKey(KeyCode.A)
+        || Input.GetKey(KeyCode.W)
+        || Input.GetKey(KeyCode.S)))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        //camera tracking for shooting
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+        if (Input.GetMouseButtonDown(0) && timer <= 0)
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+            
+            for(int i = 0; i < numBullets; i++)
+            {
+                Vector2 direction = target - myPos + new Vector2(i, i);
+                direction.Normalize();
+                foreach(GameObject p in projectile)
+                {
+                    var b = Instantiate(p, myPos, Quaternion.identity);
+                    b.GetComponent<Rigidbody2D>().velocity = (direction) * p.gameObject.GetComponent<ProjectileController>().getSpeed() * projectileSpeed;
+                    Destroy(b, 2);
+                }
+            }
+        }
+        else if (timer > 0)
         {
             timer -= Time.deltaTime;
         }
+
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Gem"))
+            {
+                GameManager.Instance.AddGems(1);
+                Destroy(collision.gameObject);
+            }
+        }
+
+    public void increaseBullets()
+    {
+        numBullets++;
+    }
+    public void increaseMovespeed()
+    {
+        movementSpeed += 2;
+    }
+    public void increaseBulletspeed()
+    {
+        projectileSpeed += .1f;
+    }
+    public void decreaseShootTime()
+    {
+        shootTime -= .2f;
     }
 }
